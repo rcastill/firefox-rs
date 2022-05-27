60935700a09f88b0fd283acbb205a51bd46788a7
Rodolfo <rodolfocastillomateluna@gmail.com>
2022-05-27 12:33:05 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Firefox
+
+Library to interface with firefox. Hacks are allowed.
+
+## Development Notes
+
+- To implement `focus_tab`, user must install `[focusTab](https://addons.mozilla.org/en-US/firefox/addon/focus_tab/)`
+    - See `assets/index.poc.html` to understand PoC of interfacing with extension
+
+- To implement tab listing, look into `assets/list-fftabs.py` as reference. Original from [gist](https://gist.github.com/tmonjalo/33c4402b0d35f1233020bf427b5539fa).
---
+++ assets/list-fftabs.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python3
+
+# gist
+# https://gist.github.com/tmonjalo/33c4402b0d35f1233020bf427b5539fa
+
+"""
+List all Firefox tabs with title and URL
+Supported input: json or jsonlz4 recovery files
+Default output: title (URL)
+Output format can be specified as argument
+"""
+
+import sys
+import pathlib
+import lz4.block
+import json
+
+path = pathlib.Path.home().joinpath('.mozilla/firefox')
+files = path.glob('*default*/sessionstore-backups/recovery.js*')
+
+try:
+    template = sys.argv[1]
+except IndexError:
+    template = '%s (%s)'
+
+for f in files:
+    b = f.read_bytes()
+    if b[:8] == b'mozLz40\0':
+        b = lz4.block.decompress(b[8:])
+    j = json.loads(b)
+    for w in j['windows']:
+        for t in w['tabs']:
+            i = t['index'] - 1
+            print(template % (
+                t['entries'][i]['title'],
+                t['entries'][i]['url']
+                ))
